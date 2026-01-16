a4cd09ae0fbf1592946d45a497380725b7329513
JFRG28 <fgrodriguez.28@gmail.com>
2026-01-16 15:39:52 -0600

    using @modelattribute notation

--- springboot-apps/src/main/java/com/jfrg/spring/app1/springboot_apps/controllers/ExampleController.java
+++ springboot-apps/src/main/java/com/jfrg/spring/app1/springboot_apps/controllers/ExampleController.java
@@ -1,8 +1,10 @@
 package com.jfrg.spring.app1.springboot_apps.controllers;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.jfrg.spring.app1.springboot_apps.models.Employee;
 
@@ -24,4 +26,12 @@ public class ExampleController {
         return "info_details";
     }
 */
+    @ModelAttribute("employees")
+    public List<Employee> getEmployees() {
+        return List.of(
+            new Employee("John", "Doe", "Evergreen terrace 123", "Developer", 30, 5551234, 1),
+            new Employee("Jane", "Smith", "Oak street 456", "Manager", 40, 5555678, 2),
+            new Employee("Alice", "Johnson", null, "Designer", 28, 5558765, 3)
+        );
+    }
 }
