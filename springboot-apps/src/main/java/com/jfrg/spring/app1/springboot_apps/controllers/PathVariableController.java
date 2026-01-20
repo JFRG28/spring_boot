e8d09ee757be92330f22e27977935a68971d77a3
JFRG28 <fgrodriguez.28@gmail.com>
2026-01-20 10:54:08 -0600

    using at_value notation to retrieve values from application.properties file

--- springboot-apps/src/main/java/com/jfrg/spring/app1/springboot_apps/controllers/PathVariableController.java
+++ springboot-apps/src/main/java/com/jfrg/spring/app1/springboot_apps/controllers/PathVariableController.java
@@ -1,18 +1,31 @@
 package com.jfrg.spring.app1.springboot_apps.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.jfrg.spring.app1.springboot_apps.models.Employee;
 
 @RestController
 @RequestMapping("/api/path-variables")
 
 public class PathVariableController {
+    
+    @Value("${config.usuario}")
+    private String usuario;
+    @Value("${config.mensaje}")
+    private String mensaje;
+    @Value("${config.codigo}")
+    private int codigo;
+    @Value("${config.valores}")
+    private String[] valores;
+
     @GetMapping("/page1/{msg}")
     public ParamDTO getPage1(@PathVariable String msg) {
         ParamDTO param1 = new ParamDTO();
@@ -25,4 +38,13 @@ public class PathVariableController {
         return paramEmployee;
     }
 
+    @GetMapping("/values")
+    public Map<String, Object> getValues() {
+        Map<String, Object>json = new HashMap<>();
+        json.put("usuario", usuario);
+        json.put("mensaje", mensaje);
+        json.put("código", codigo);
+        json.put("valores", valores);
+        return json;
+    }
 }
