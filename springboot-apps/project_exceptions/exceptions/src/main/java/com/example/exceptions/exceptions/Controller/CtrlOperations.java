f18b579f421b25c3cda0a639f826fe874b89ed5e
JFRG28 <fgrodriguez.28@gmail.com>
2026-01-21 16:42:02 -0600

    using NumberFormatException

--- springboot-apps/project_exceptions/exceptions/src/main/java/com/example/exceptions/exceptions/Controller/CtrlOperations.java
+++ springboot-apps/project_exceptions/exceptions/src/main/java/com/example/exceptions/exceptions/Controller/CtrlOperations.java
@@ -1,6 +1,7 @@
 package com.example.exceptions.exceptions.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,9 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CtrlOperations {
 
     @GetMapping("/divide")
+/*
     public String divide() {
         float value = 10 / 0;
         return "Result: " + value;
     }
-    
+*/
+    public String divide(@RequestParam String paramNumber) {
+        try {
+            float value = Float.parseFloat(paramNumber);
+            float result = 10 / value;
+            return "Result: " + result;
+        } catch (NumberFormatException ERROR) {
+            return "Error: Invalid number format.";
+        } catch (ArithmeticException ERROR) {
+            return "Error: Division by zero not allowed.";
+        }
+    }
 }
